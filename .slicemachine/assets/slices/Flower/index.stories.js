c502702ad9b1baa7019f19b47d31017d35ab96bf
Arni Ingi Johannesson <arni.ingi@skaginn3x.com>
2022-04-12 14:36:57 +0000

    Eg skil ekki neitt i rassgat

---
+++ .slicemachine/assets/slices/Flower/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Flower';
+
+export default {
+  title: 'slices/Flower'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"flower","items":[],"primary":{"title":[{"type":"heading1","text":"Brand sexy applications","spans":[]}],"description":[{"type":"paragraph","text":"Esse nisi proident cillum ut minim ex.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
